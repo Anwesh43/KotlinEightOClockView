d8a0b8a90118f25aeb55d2c7d23a3b5e6379320e
Anwesh43 <anweshthecool0@gmail.com>
2018-04-24 05:57:01 +0530

    adding fullscreen support

--- app/src/main/java/ui/anwesome/com/kotlineightoclockview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlineightoclockview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlineightoclockview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.eightoclockview.EightOClockView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         EightOClockView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
